36447a222f957395a2dcad306c9634208026dfeb
Alan Yoshida <alanyoshida88@gmail.com>
2017-08-24 20:22:07 -0300

    Alert component with emit

---
+++ src/components/Alert.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="alert" v-if="isVisible">
+    <slot></slot>
+    <button type="button" name="button" @click="close()">X</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+  data(){
+    return {
+      isVisible: this.visible
+    }
+  },
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+      required: true
+    }
+  },
+  watch: {
+    visible(value){
+      this.isVisible = value;
+    }
+  },
+  methods: {
+    close(){
+      this.isVisible = false;
+      this.$emit('alert-close', true)
+    }
+  }
+}
+</script>
+
+<style>
+  .alert {
+    padding: 15px;
+    margin: 20px;
+    background-color: green;
+    color: white;
+    border-radius: 5px;
+  }
+  .button {
+    float: right;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <router-link :to="{name:'teste'}">teste denovo</router-link>
+    <alert :visible="visibleAlert" @alert-close="notification()">{{msg}}</alert>
+    <input type="text" name="" v-model="msg">
+    <button type="button" name="button" @click="toggle()">Toggle alert</button>
+  </div>
+</template>
+
+<script>
+  import Alert from './Alert';
+  export default {
+    name: 'Home',
+    components: {
+      Alert
+    },
+    data(){
+        return {
+          msg: 'mensagem do alerta',
+          visibleAlert: false
+        }
+    },
+    methods: {
+      toggle(){
+        this.visibleAlert = !this.visibleAlert;
+      },
+      notification(){
+        this.msg = "ALERTA FECHADO";
+      }
+    }
+  }
+</script>
